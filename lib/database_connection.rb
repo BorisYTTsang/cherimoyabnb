edeaf30af25e6496639b16b732ee822ae8e2769e
AntVelc <t.velciu@gmail.com>
2022-09-05 16:57:41 +0100

    login page html

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -35,6 +35,5 @@ class DatabaseConnection
       '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
     end
     @connection.exec_params(query, params)
-    
   end
 end
