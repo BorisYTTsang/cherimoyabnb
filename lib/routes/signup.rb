e386f22ec96d673cc55fdc7642171870f2c3a268
BorisYTTsang <boris.yttsang@outlook.com>
2022-09-08 04:04:33 +0100

    CSS draft done, RSpec notices swabbed

--- lib/routes/signup.rb
+++ lib/routes/signup.rb
@@ -2,18 +2,15 @@ class Application < Sinatra::Base
 
     get "/signup" do
         return erb(:signup2)
-
     end
 
     post "/signup" do
-
         repo = UserRepository.new
         new_user = User.new
         new_user.name = params[:name]
         new_user.email = params[:email]
         new_user.password = params[:password]
         repo.create(new_user)
-        return erb(:login)
-
+        return erb(:login2)
     end
 end
