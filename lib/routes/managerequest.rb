ccc8f1e0399c2220b5115266a88e42b85efefbe1
nickwlong <nickwlong@gmail.com>
2022-09-08 16:06:55 +0100

    Request manager page and confirmbooking button now working -m 'endless merge commits'

--- lib/routes/managerequest.rb
+++ lib/routes/managerequest.rb
@@ -9,34 +9,37 @@ class Application < Sinatra::Base
     space_id = params[:space_id]
     request_id = params[:request_id]
 
-    # @space = space_repo.find(space_id)
-    # @request = request_repo.find(request_id)
-    # @user = user_repo.find(@request.owner_id)
-    # @booker = user_repo.find(request.booker_id)
+    @space = space_repo.find(space_id)
+    @selectedrequest = request_repo.find(request_id)
+    @user = user_repo.find(@selectedrequest.owner_id)
+    @booker = user_repo.find(@selectedrequest.booker_id)
 
     return erb(:managerequest)
   end
     
-  # post '/make-booking' do
-  #   @success = ''
-  #   space_id = params[:space_id]
-  #   request_id = params[:request_id]
+  post '/confirmbooking' do
+    space_id = params[:space_id]
+    request_id = params[:request_id]
 
-  #   @space = space_repo.find(space_id)
-  #   @request = request_repo.find(request_id)
+    @space = space_repo.find(space_id)
+    @selectedrequest = request_repo.find(request_id)
+    @user = user_repo.find(@selectedrequest.owner_id)
+    @booker = user_repo.find(@selectedrequest.booker_id)
 
-  #   new_booking = Booking.new
-  #   new_booking.space_id = @request.space_id
-  #   new_booking.unavailable_from = @request.date_from
-  #   new_booking.unavailable_to = @request.date_to
-  #   new_booking.reason = 'booking'
-  #   new_booking.booker_id = @request.booker_id
+    new_booking = Booking.new
+    new_booking.space_id = @selectedrequest.space_id
+    new_booking.unavailable_from = @selectedrequest.date_from
+    new_booking.unavailable_to = @selectedrequest.date_to
+    new_booking.reason = 'booking'
+    new_booking.booker_id = @selectedrequest.booker_id
 
-  #   if repo.create(new_booking)
-  #     @success = true
-  #   else
-  #     @success = false
-  #   end
-  #   return erb(:makebooking)
-  # end
+    if booking_repo.create(new_booking)
+      @success = true
+      return erb(:managerequest)
+    else
+      @success = false
+      return erb(:managerequest)
+    end
+  end
+
 end
