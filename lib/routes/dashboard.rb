18033fdce4fbb862481e249c750e5f9a43250d97
nickwlong <nickwlong@gmail.com>
2022-09-09 13:51:36 +0100

    changing filter to a get request

--- lib/routes/dashboard.rb
+++ lib/routes/dashboard.rb
@@ -3,29 +3,24 @@ class Application < Sinatra::Base
   space_repo = SpaceRepository.new
 
   get "/dashboard" do
-      if session[:user_id].nil?
-        redirect "/login"
-      else
+      (redirect "/login") if session[:user_id].nil?
+      if params[:available_from].nil?
         @user_name = session[:user_name]
-        @spaces_booked
         @spaces = space_repo.all
+        return erb(:dashboard)
+      else
+        @spaces = []
+        space_repo = SpaceRepository.new
+        available_from = params[:available_from]
+        available_to = params[:available_to]
+        max_price = params[:max_price]
+    
+        @user_name = session[:user_name]
+        
+        @filtered_spaces = space_repo.filter(available_from, available_to, max_price)
+    
         return erb(:dashboard)
       end
   end
 
-  post "/dashboard" do
-
-    space_repo = SpaceRepository.new
-    available_from = params[:available_from]
-    available_to = params[:available_to]
-    max_price = params[:max_price]
-
-    @user_name = session[:user_name]
-    
-    @filtered_spaces = space_repo.filter(available_from, available_to, max_price)
-
-    redirect "/dashboard"
-    # return erb(:dashboard)
-  end
-  
 end
